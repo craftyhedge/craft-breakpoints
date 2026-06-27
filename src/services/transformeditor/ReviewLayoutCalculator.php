294931fe53c4d6300420b1ad7f7f7f9665793000
Crafty Hedge <martinsplugins@gmail.com>
2026-06-27 12:15:57 +0100

    placeholder improvement

--- src/services/transformeditor/ReviewLayoutCalculator.php
+++ src/services/transformeditor/ReviewLayoutCalculator.php
@@ -14,6 +14,11 @@ final class ReviewLayoutCalculator
     private const INITIAL_PLACEHOLDER_FALLBACK_HEIGHT = 800;
     private const INITIAL_PLACEHOLDER_DEFAULT_RATIO_WIDTH = 3;
     private const INITIAL_PLACEHOLDER_DEFAULT_RATIO_HEIGHT = 2;
+    private const INITIAL_PLACEHOLDER_ICON_VIEWBOX_X = -1.0;
+    private const INITIAL_PLACEHOLDER_ICON_VIEWBOX_Y = -1.0;
+    private const INITIAL_PLACEHOLDER_ICON_VIEWBOX_WIDTH = 23.947266;
+    private const INITIAL_PLACEHOLDER_ICON_VIEWBOX_HEIGHT = 17.894531;
+    private const INITIAL_PLACEHOLDER_ICON_PATH = 'M 7.3339844,-1 C 6.628056,-1 6.0371094,-0.40905336 6.0371094,0.296875 V 3.9863281 H 3.7324219 c -0.6715314,0 -1.234375,0.564797 -1.234375,1.2363281 V 8.609375 H 0.15820312 C -0.4706666,8.609375 -1,9.1367553 -1,9.765625 v 5.972656 c 0,0.62887 0.52933379,1.15625 1.15820312,1.15625 H 8.1269531 c 0.6288693,0 1.1582032,-0.52738 1.1582032,-1.15625 v -2.271484 h 5.2539067 c 0.671531,0 1.234375,-0.562844 1.234375,-1.234375 V 9.4433594 h 5.878906 c 0.705928,0 1.294922,-0.5909461 1.294922,-1.296875 V 0.296875 C 22.947266,-0.40905395 22.358272,-1 21.652344,-1 Z M 7.4667969,0.4296875 H 21.517578 v 7.5820313 h -5.74414 V 5.2226562 c 0,-0.6715311 -0.562844,-1.2363281 -1.234375,-1.2363281 H 7.4667969 Z M 3.9277344,5.4179687 H 14.34375 V 12.037109 H 3.9277344 Z M 0.4296875,10.039063 h 2.0683594 v 2.193359 c 0,0.671531 0.5628438,1.234375 1.234375,1.234375 h 4.1230468 v 1.998047 H 0.4296875 Z';
 
     /**
      * @param array<int, array<string, mixed>> $rows
@@ -339,13 +344,22 @@ final class ReviewLayoutCalculator
         ?string $autoDimension,
     ): string {
         [$boxWidth, $boxHeight] = self::resolveInitialPreviewBoxDimensions($width, $height, $autoDimension);
+        $iconWidth = max(72.0, min($boxWidth * 0.26, $boxHeight * 0.36, 260.0));
+        $iconScale = $iconWidth / self::INITIAL_PLACEHOLDER_ICON_VIEWBOX_WIDTH;
+        $iconHeight = self::INITIAL_PLACEHOLDER_ICON_VIEWBOX_HEIGHT * $iconScale;
+        $iconX = (($boxWidth - $iconWidth) / 2) - (self::INITIAL_PLACEHOLDER_ICON_VIEWBOX_X * $iconScale);
+        $iconY = (($boxHeight - $iconHeight) / 2) - (self::INITIAL_PLACEHOLDER_ICON_VIEWBOX_Y * $iconScale);
 
         $svg = sprintf(
-            '<svg xmlns="http://www.w3.org/2000/svg" width="%1$d" height="%2$d" viewBox="0 0 %1$d %2$d" role="img" aria-label="Placeholder"><rect width="100%%" height="100%%" fill="#e7edf5"/><rect x="1" y="1" width="%3$d" height="%4$d" fill="none" stroke="#98a9be" stroke-width="2"/></svg>',
+            '<svg xmlns="http://www.w3.org/2000/svg" width="%1$d" height="%2$d" viewBox="0 0 %1$d %2$d" role="img" aria-label="Preview pending"><defs><linearGradient id="bpts-placeholder-bg" x1="0" y1="0" x2="1" y2="1"><stop offset="0" stop-color="#f4f7fb"/><stop offset="1" stop-color="#e7edf5"/></linearGradient><pattern id="bpts-placeholder-stripes" width="28" height="28" patternUnits="userSpaceOnUse" patternTransform="rotate(35)"><rect width="12" height="28" fill="#ffffff" opacity="0.28"/></pattern></defs><rect width="100%%" height="100%%" fill="url(#bpts-placeholder-bg)"/><rect width="100%%" height="100%%" fill="url(#bpts-placeholder-stripes)" opacity="0.55"/><rect x="1" y="1" width="%3$d" height="%4$d" fill="none"/><path d="%5$s" fill="#8fa1b8" opacity="0.58" transform="translate(%6$.3F %7$.3F) scale(%8$.5F)"/></svg>',
             $boxWidth,
             $boxHeight,
             max(1, $boxWidth - 2),
             max(1, $boxHeight - 2),
+            self::INITIAL_PLACEHOLDER_ICON_PATH,
+            $iconX,
+            $iconY,
+            $iconScale,
         );
 
         return 'data:image/svg+xml,' . rawurlencode($svg);
